bf07738a625493caa24d6552d9d33550edcf7452
JOBFER <gbringashotmail@hotmail.com>
2017-09-17 10:02:41 +0200

    COMMIT 1 desde BASH con RAMATEST...

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Inicio"); 
+		System.out.println("Inicio");
+		System.out.println("COMMIT 1 desde BASH con RAMATEST");
 	}
 
 }
