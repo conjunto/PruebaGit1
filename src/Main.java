03a4d37f58635eb654b54007d3caf611276cd3b6
JOBFER <gbringashotmail@hotmail.com>
2017-09-17 10:31:03 +0200

    COMMIT 2 desde BASH con MASTER despues del MERGE...

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Inicio");
 		System.out.println("COMMIT 1 desde BASH con RAMATEST");
+		System.out.println("COMMIT 2 desde BASH con MASTER despues del MERGE");
 	}
 
 }
